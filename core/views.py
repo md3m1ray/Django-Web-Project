2f2469830331f66dddeb66c69022c1a0d618e05f
Muhammet Demiray <md3m1ray@gmail.com>
2024-02-11 21:09:03 +0300

    refactor: add contact.html and create urls

--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def cobtact(request):
+    return render(request, 'contact.html')
