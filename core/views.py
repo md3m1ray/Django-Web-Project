b6adc9cd5502aaa0979432a3e46f9f72b06cb10a
Muhammet Demiray <md3m1ray@gmail.com>
2024-02-17 21:20:31 +0300

    refactor: add layout.html and other pages

--- core/views.py
+++ core/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
-def cobtact(request):
+def contact(request):
     return render(request, 'contact.html')
