e107365f271e893bba2c7330fa404e7f9965c58a
Muhammet Demiray <md3m1ray@gmail.com>
2024-02-25 01:55:51 +0300

    feat: create contact app

---
+++ contact/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+
+# Create your views here.
+
+def contact_form(request):
+    context = {'success': True, 'message': 'Contact form is valid!'}
+    return JsonResponse(context)
+
+def contact(request):
+    return render(request, 'contact.html')
